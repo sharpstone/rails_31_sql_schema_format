04d830ee28606785b53764273d6a9e73c3da0128
schneems <richard.schneeman@gmail.com>
2017-03-27 09:33:15 -0500

    first

---
+++ config/application.rb
@@ -0,0 +1,9 @@
+require_relative 'boot'
+require 'rails/all'
+Bundler.require(*Rails.groups)
+
+module MicroRails
+  class Application < Rails::Application
+    config.active_record.schema_format = :ruby
+  end
+end
---
+++ test/da_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+class DaTest < ActiveSupport::TestCase
+  test "test_true" do
+    assert_equal true, true
+  end
+end
